f374a6dfc54023a2dcff2676546bb62a6e4949dc
Solaiman Islam Naiem <59079456+Naiem890@users.noreply.github.com>
2023-07-17 01:23:15 +0600

    baseurl changed

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const isProduction = process.env.NODE_ENV === "production";
+const baseURL = isProduction
+  ? "https://print-korun.onrender.com"
+  : "http://localhost:5000";
+
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL,
   headers: {
     "Content-Type": "application/json",
   },
