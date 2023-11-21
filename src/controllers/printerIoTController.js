0e9389e927b78d6e5ee3449a300d17c993ebc2e9
shohanshovo11 <101856535+shohanshovo11@users.noreply.github.com>
2023-11-21 22:58:15 +0600

    modified printerIoTController -added modify pritnerIoT -added delete printerIoT

--- src/controllers/printerIoTController.js
+++ src/controllers/printerIoTController.js
@@ -69,5 +69,88 @@ router.get("/:printerIoTId", validateToken, async (req, res) => {
     res.status(500).json({ error: "Failed to retrieve printer(s)" });
   }
 });
+// Update an existing printer
+router.put(
+  "/:printerIoTId",
+  validateToken,
+  checkAdminRole,
+  async (req, res) => {
+    try {
+      const { printerIoTId } = req.params;
+      const {
+        name,
+        location,
+        googleMapLink,
+        ip,
+        colorPrintPrice,
+        bAndWPrintPrice,
+      } = req.body;
+      // Find the printer by ID
+      const existingPrinterIoT = await PrinterIoT.findById(printerIoTId);
+      console.log(existingPrinterIoT, "Shovo");
+      if (!existingPrinterIoT) {
+        return res.status(404).json({ error: "Printer not found" });
+      }
 
+      // Update the printer details
+      existingPrinterIoT.name ? (existingPrinterIoT.name = name) : null;
+      existingPrinterIoT.location
+        ? (existingPrinterIoT.location = location)
+        : null;
+      existingPrinterIoT.googleMapLink
+        ? (existingPrinterIoT.googleMapLink = googleMapLink)
+        : null;
+      existingPrinterIoT.ip ? (existingPrinterIoT.ip = ip) : null;
+      existingPrinterIoT.colorPrintPrice
+        ? (existingPrinterIoT.colorPrintPrice = colorPrintPrice)
+        : null;
+      existingPrinterIoT.BWPrintPrice
+        ? (existingPrinterIoT.BWPrintPrice = bAndWPrintPrice)
+        : null;
+
+      // Save the updated printer to the database
+      const updatedPrinterIoT = await existingPrinterIoT.save();
+
+      res.status(200).json({
+        printerIoT: updatedPrinterIoT,
+        message: "Printer updated successfully",
+      });
+    } catch (error) {
+      console.error("Error updating printer:", error);
+      res.status(500).json({ error: "Failed to update printer" });
+    }
+  }
+);
+
+router.delete(
+  "/:printerIoTId",
+  validateToken,
+  checkAdminRole,
+  async (req, res) => {
+    try {
+      const { printerIoTId } = req.params;
+
+      // Find the printer by ID and delete it
+      const deletedPrinterIoT = await PrinterIoT.findByIdAndDelete(
+        printerIoTId
+      );
+
+      if (!deletedPrinterIoT) {
+        return res.status(404).json({ error: "Printer not found" });
+      }
+
+      res.status(200).json({
+        printerIoT: {
+          _id: deletedPrinterIoT._id, // Access the _id directly
+          name: deletedPrinterIoT.name,
+          // Include other fields as needed
+        },
+        message: "Printer deleted successfully",
+      });
+    } catch (error) {
+      console.error("Error deleting printer:", error);
+      res.status(500).json({ error: "Failed to delete printer" });
+    }
+  }
+);
 module.exports = router;
